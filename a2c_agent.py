4d3b8ea8d9c6dacfd00035879815b327fbe9851a
Saminda Abeyruwan <saminda@saminda.local>
2018-10-21 19:19:25 -0700

    update

--- a2c_agent.py
+++ a2c_agent.py
@@ -1,4 +1,17 @@
-"""A2C agent for Reacher task"""
+"""
+A2C agent for Reacher task
+The implementation is inspired by: https://github.com/ShangtongZhang/DeepRL
+
+Saminda Abeyruwan has modified the code to work with Continuous Control Project
+and made his own modifications, optimizations, and parameter tuning.  The
+authors copyright is available herewith as a reference.
+"""
+#######################################################################
+# Copyright (C) 2017 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
 
 import argparse
 import logging
@@ -7,15 +20,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+from collections import deque
 from unityagents import UnityEnvironment
+import matplotlib
+
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
 
 class Config:
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    def __int__(self):
+    def __init__(self):
         self.parser = argparse.ArgumentParser()
-        self.history_length = 4
         self.num_workers = 20
         self.discount = 0.99
         self.use_gae = True
@@ -23,12 +40,17 @@ class Config:
         self.entropy_weight = 0.01
         self.rollout_length = 5
         self.gradient_clip = 5
-        self.max_steps = int(2e7)
+        self.value_loss_weight = 1.0
+        self.scores_maxlen = 100
         self.state_dim = 33
         self.action_dim = 4
         self.file_name = "<file_name>"
         self.logger = logging.getLogger('a2c_agent')
-        self.eval_episodes = 2
+        self.eval_interval = 0
+        self.eval_episodes = 10
+        self.save_interval = 10
+        self.train_episodes = 150
+        self.train_mode = True
 
     def add_argument(self, *args, **kwargs):
         self.parser.add_argument(*args, **kwargs)
@@ -105,7 +127,7 @@ class GaussianActorCriticNet(nn.Module):
 
 
 class FCBody(nn.Module):
-    def __init__(self, state_dim, hidden_units=(64, 64), gate=F.relu):
+    def __init__(self, state_dim, hidden_units=(200, 200), gate=F.relu):
         super(FCBody, self).__init__()
         dims = (state_dim,) + hidden_units
         self.layers = nn.ModuleList(
@@ -119,6 +141,15 @@ class FCBody(nn.Module):
         return x
 
 
+class DummyBody(nn.Module):
+    def __init__(self, state_dim):
+        super(DummyBody, self).__init__()
+        self.feature_dim = state_dim
+
+    def forward(self, x):
+        return x
+
+
 class UnityEnvironmentTask:
 
     def __init__(self, file_name, train_mode=True):
@@ -128,6 +159,7 @@ class UnityEnvironmentTask:
         self.env_info = self.env.reset(train_mode=train_mode)[self.brain_name]
         self.num_agents = len(self.env_info.agents)
         self.action_dim = self.brain.vector_action_space_size
+        print('Brain name:', self.brain_name)
         print('Number of agents:', self.num_agents)
         print('Size of each action:', self.action_dim)
 
@@ -142,21 +174,28 @@ class UnityEnvironmentTask:
         terminals = env_info.local_done
         if np.any(terminals):
             next_states = self.reset()
-        return next_states, rewards, terminals, env_info
+        return next_states, np.asarray(rewards, dtype=np.float32), np.asarray(terminals, dtype=np.float32), env_info
+
+    def close(self):
+        self.env.close()
 
 
 class A2CAgent:
     def __init__(self, config):
         self.config = config
-        self.task = UnityEnvironmentTask(file_name=config.file_name)
-        self.network = GaussianActorCriticNet(config.state_dim, config.action_dim, actor_body=FCBody(config.state_dim),
+        self.task = UnityEnvironmentTask(file_name=config.file_name, train_mode=config.train_mode)
+        self.network = GaussianActorCriticNet(config.state_dim, config.action_dim,
+                                              phi_body=DummyBody(config.state_dim),
+                                              actor_body=FCBody(config.state_dim),
                                               critic_body=FCBody(config.state_dim))
         self.optimizer = torch.optim.RMSprop(self.network.parameters(), lr=0.0007)
         self.total_steps = 0
         self.states = self.task.reset()
-
-        self.episode_rewards = []
         self.online_rewards = np.zeros(config.num_workers)
+
+        self.total_episodes = 0
+        self.scores_deque = deque(maxlen=config.scores_maxlen)
+        self.scores_global = []
 
     def step(self):
         config = self.config
@@ -166,10 +205,14 @@ class A2CAgent:
             actions, log_probs, entropy, values = self.network(states)
             next_states, rewards, terminals, _ = self.task.step(actions.detach().cpu().numpy())
             self.online_rewards += rewards
-            for i, terminal in enumerate(terminals):
-                if terminals[i]:
-                    self.episode_rewards.append(self.online_rewards[i])
+
+            if np.any(terminals):
+                score = np.mean(self.online_rewards)
+                self.scores_deque.append(score)
+                self.scores_global.append(score)
+                for i, terminal in enumerate(terminals):
                     self.online_rewards[i] = 0
+                self.total_episodes += 1
 
             rollout.append([log_probs, values, actions, rewards, 1 - terminals, entropy])
             states = next_states
@@ -220,19 +263,20 @@ class A2CAgent:
         self.network.load_state_dict(state_dict)
 
     def eval_step(self, states):
-        raise Exception('eval_step not implemented')
+        with torch.no_grad():
+            actions, _, _, _ = self.network(states)
+        return actions.detach().cpu().numpy()
 
     def eval_episode(self):
-        env = self.task = UnityEnvironmentTask(file_name=self.config.file_name, train_mode=False)
-        states = env.reset()
-        total_rewards = 0
+        states = self.task.reset()
+        total_rewards = np.zeros(self.config.num_workers)
         while True:
             action = self.eval_step(states)
-            states, rewards, dones, _ = env.step(action)
+            states, rewards, dones, _ = self.task.step(action)
             total_rewards += rewards
             if np.any(dones):
                 break
-        return total_rewards
+        return np.mean(total_rewards)
 
     def eval_episodes(self):
         rewards = []
@@ -241,25 +285,50 @@ class A2CAgent:
         self.config.logger.info('evaluation episode return: %f(%f)' % (
             np.mean(rewards), np.std(rewards) / np.sqrt(len(rewards))))
 
+    def close(self):
+        self.task.close()
 
-def run_steps(agent):
+
+def train_agent(agent):
     Util.random_seed()
     config = agent.config
     agent_name = agent.__class__.__name__
+    prev_total_episodes = agent.total_episodes
     t0 = time.time()
     while True:
-        if config.save_interval and not agent.total_steps % config.save_interval:
-            agent.save('data/model-%s-%s-%s.bin' % (agent_name, config.task_name, config.tag))
-        if config.log_interval and not agent.total_steps % config.log_interval and len(agent.episode_rewards):
-            rewards = agent.episode_rewards
-            agent.episode_rewards = []
-            config.logger.info('total steps %d, returns %.2f/%.2f/%.2f/%.2f (mean/median/min/max), %.2f steps/s' % (
-                agent.total_steps, np.mean(rewards), np.median(rewards), np.min(rewards), np.max(rewards),
-                config.log_interval / (time.time() - t0)))
+        if config.save_interval and not agent.total_episodes % config.save_interval:
+            agent.save('model-{}.bin'.format(agent_name))
+        if agent.total_episodes > prev_total_episodes:
+            config.logger.info('total episodes %d, returns(last %d) %.2f/%.2f (mean/median), %.2f s' % (
+                agent.total_episodes, config.scores_maxlen, np.mean(agent.scores_deque), np.median(agent.scores_deque),
+                (time.time() - t0)))
             t0 = time.time()
-        if config.eval_interval and not agent.total_steps % config.eval_interval:
+            prev_total_episodes = agent.total_episodes
+        if config.eval_interval and not agent.total_episodes % config.eval_interval:
             agent.eval_episodes()
-        if config.max_steps and agent.total_steps >= config.max_steps:
+        if config.train_episodes and agent.total_episodes >= config.train_episodes:
             agent.close()
             break
         agent.step()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(np.arange(1, len(agent.scores_global) + 1), agent.scores_global)
+    plt.ylabel('Score')
+    plt.xlabel('Episode #')
+    plt.savefig('scores.png')
+
+
+def eval_agent(agent):
+    agent.config.logger.info('Evaluate agent')
+    agent_name = agent.__class__.__name__
+    agent.load('model-{}.bin'.format(agent_name))
+    agent.eval_episodes()
+
+
+
+config = Config()
+config.file_name = "/Users/saminda/Udacity/DRLND/Sim/Reacher20/Reacher.app"
+config.train_mode = False
+#train_agent(A2CAgent(config))
+eval_agent(A2CAgent(config))
